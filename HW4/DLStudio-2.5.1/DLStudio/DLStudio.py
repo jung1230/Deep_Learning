27675cfe320ca90b8db36e98d69dce52faf13d98
jung1230 <alanchen20072@gmail.com>
2025-02-07 13:18:54 -0500

    cont HW4

--- HW4/DLStudio-2.5.1/DLStudio/DLStudio.py
+++ HW4/DLStudio-2.5.1/DLStudio/DLStudio.py
@@ -3156,7 +3156,7 @@ class DLStudio(object):
             """
             def __init__(self):
                 super(DLStudio.ExperimentsWithCIFAR.Net, self).__init__()
-                self.conv1 = nn.Conv2d(3, 6, 5)
+                self.conv1 = nn.Conv2d(3, 6, 5) # input channel, output channel, kernel size
                 self.conv2 = nn.Conv2d(6, 16, 5)
                 self.fc1 = nn.Linear(16 * 5 * 5, 120)
                 self.fc2 = nn.Linear(120, 84)
